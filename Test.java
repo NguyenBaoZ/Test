294d921c02353dde9fc9539dee8033851b70a173
Nguyen Bao <mac@MacBook-Pro.local>
2021-04-01 11:10:06 +0700

    In thong tin moi

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello A");
+        System.out.println("Welcome to my world");
     }
-} 
+}
